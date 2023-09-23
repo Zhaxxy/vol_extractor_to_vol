ea71ca2052ab38f8ac52f87517b5907d889ce16d
Zhaxxy <67915404+Zhaxxy@users.noreply.github.com>
2023-09-23 21:06:42 +0100

    Update vol_extractor_to_vol.py

--- vol_extractor_to_vol.py
+++ vol_extractor_to_vol.py
@@ -1,8 +1,13 @@
 import zlib
 import struct
 import argparse
+import json
+from pathlib import Path
 
 VOL_HEADER = b'P\xf0w\xd1\x01\x01\x00\x00'
+
+with open(Path(Path(__file__).parent,'nvft_offsets_ps4.json')) as f:
+    NVFT_OFFSETS = json.load(f)
 
 def extract_vol(vol_file: bytes) -> bytes:
     if not vol_file.startswith(VOL_HEADER):
@@ -22,8 +27,18 @@ def extract_vol(vol_file: bytes) -> bytes:
     return decompressed_data
 
 
-def to_vol(normal_file: bytes) -> bytes:
-    compressed_data = zlib.compress(normal_file,level=9,wbits=-15)
+def to_vol(normal_file: bytes, temp_patch_nvft: str = None) -> bytes:
+    compressed_data = zlib.compress(normal_file,wbits=-15)
+    if temp_patch_nvft:
+        compressed_size_offset, decompressed_size_offfset = NVFT_OFFSETS[Path(temp_patch_nvft[0]).name]
+        
+        with open(temp_patch_nvft[1],'rb+') as f:
+            f.seek(compressed_size_offset)
+            f.write(struct.pack('<I',len(compressed_data)))
+            f.seek(0)
+            f.seek(decompressed_size_offfset)
+            f.write(struct.pack('<I',len(normal_file)))
+    
     return VOL_HEADER + struct.pack('<2I',len(compressed_data),len(normal_file)) + compressed_data
 
 def main(args=None):
@@ -32,15 +47,18 @@ def main(args=None):
     parser.add_argument('input_file',help='Input file to ethier extract or pack')
     parser.add_argument('output_file',help='Output file of new .vol file or file from .vol')
     
-    parser.add_argument('-p', '--pack_to_vol', help='Do you want to pack to a .vol file',action='store_true')
+    parser.add_argument('-p', '--pack_to_vol', help='The path to the nvft file, to patch in new offsets', nargs='?', type = str, const=False)
     
     args = parser.parse_args()
+
+    if args.pack_to_vol is False:
+        raise ValueError('Please provide a path to the nvft file!')
     
     with open(args.input_file,'rb') as f:
         data = f.read()
     
     if args.pack_to_vol:
-        new_data = to_vol(data)
+        new_data = to_vol(data,(Path(args.output_file).name,args.pack_to_vol))
     else:
         new_data = extract_vol(data)
     
