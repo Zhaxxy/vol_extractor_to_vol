24703fa364e6b3479dd90feecab2a9214669201e
Zhaxxy <67915404+Zhaxxy@users.noreply.github.com>
2023-10-08 14:47:26 +0100

    gave a name to the hash function

--- vol_extractor_to_vol.py
+++ vol_extractor_to_vol.py
@@ -1,27 +1,11 @@
-from __future__ import annotations
 import struct
-import os
 import zlib
-import json
-from typing import NamedTuple, Sequence
-from io import BytesIO
-from pathlib import Path
-import logging
-import argparse
-
-VOL_HEADER = b'P\xf0w\xd1\x01\x01\x00\x00'
-
-with open(Path(Path(__file__).parent,'nvft_offsets_ps4.json')) as f:
-    NVFT_OFFSETS = json.load(f) 
-
-
-def decode_num(number: bytes, /) -> int:
-    return struct.unpack('<I',number)[0]
 
 def uint32(value: int, /) -> int:
     return value & 0xFFFFFFFF
 
-def scurse_hash(raw_data: bytes, /) -> int:
+
+def scurse_hash(raw_data: bytes, /):
     """
     thx to https://github.com/algmyr/ alot for helping making this function
     if you can recongise the hash aloghorthim and have a better python implemention let me know!, 
@@ -96,255 +80,88 @@ def scurse_hash(raw_data: bytes, /) -> int:
     return result
 
 
-def decompress_vol(vol_file: bytes) -> bytes:
-    if not vol_file.startswith(VOL_HEADER):
-        raise ValueError('Invalid .vol file')
+def check_save(save_bytes: bytes, /):
+    SOMEMAGICVALUE = 1224793212
+    og_hash = struct.unpack("<I", save_bytes[:4])[0]
 
-    compressed_data_size, decompressed_data_size = struct.unpack('<2I',vol_file[8: 8 + 8])
-    compressed_data = vol_file[0x10:]
-    
-    if len(compressed_data) != compressed_data_size:
-        raise ValueError('Invalid compressed data size')
-    
-    decompressed_data = zlib.decompress(compressed_data,wbits=-15)
-    
-    if len(decompressed_data) != decompressed_data_size:
-        raise ValueError('Invalid decompressed data size')
-    
-    return decompressed_data
+    return uint32(scurse_hash(save_bytes[4:]) + SOMEMAGICVALUE) == og_hash
 
 
-def compress_vol(normal_file: bytes, temp_patch_nvft: str = None) -> bytes:
-    compressed_data = zlib.compress(normal_file,wbits=-15)
-    if temp_patch_nvft:
-        compressed_size_offset, decompressed_size_offfset = NVFT_OFFSETS[Path(temp_patch_nvft[0]).name]
+def decode_save(save_bytes: bytes, /, check_the_save: bool = True) -> bytes:
+    WBITS = -15
+    if (not check_save(save_bytes)) and check_the_save:
+        raise ValueError('Hash mismatch')
         
-        with open(temp_patch_nvft[1],'rb+') as f:
-            f.seek(compressed_size_offset)
-            f.write(struct.pack('<I',len(compressed_data)))
-            f.seek(0)
-            f.seek(decompressed_size_offfset)
-            f.write(struct.pack('<I',len(normal_file)))
-    
-    return VOL_HEADER + struct.pack('<2I',len(compressed_data),len(normal_file)) + compressed_data
+    return zlib.decompress(save_bytes[4:],wbits=WBITS)
 
 
-class VolumeFileLink(NamedTuple):
-    """
-    a file link or entry in the vol file, it is 0x18 bytes long and contains a hash of the filename, the absoulte offset of the file data in the volume and the file data size
-    """
-    filename_hash: int
-    #unknown_number: int
-    file_data_start: int
-    #unknown_number2: int
-    #unknown_number3: int
-    file_data_size: int
-    
-    def __bytes__(self):
-        return struct.pack('<6I',self.filename_hash,0,self.file_data_start,0,0,self.file_data_size)
-    
-    @classmethod
-    def from_bytes(cls, file_link_bytes: int):
-        filename_hash,_,file_data_start,_,_,file_data_size = struct.unpack('<6I',file_link_bytes)
-        return cls(filename_hash,file_data_start,file_data_size)
-    
-    def __hash__(self):
-        return self.filename_hash
-    
-    def __eq__(self: 'VolumeFileLink' ,other: VolumeFileLink | int):
-        if isinstance(other,int):
-            return self.filename_hash == other
-        else:
-            return tuple(self) == tuple(other)
-
-
-def build_vol_header(datablocks_offset: int,decompressed_data_size: int, filenames_hashes: Sequence[int]) -> bytes | int:
-    """
-    build the header and buckets for the voluem, the second return value is the buckets_size, which is used to calculuate the hash jump
-    """
-    # buckets = list(range(0,len(filenames_hashes)+1,5))
-    # if len(buckets) < 2:
-        # buckets = [0,len(filenames_hashes)]
-    
-    
-    # if len(buckets) > 2 and not len(buckets) % 2:
-        # buckets.pop(2)
-    
-    # for index,_ in enumerate(buckets):
-        # if index == 0:
-            # continue
-        # buckets[index] -= 1
-    
-    # buckets[-1] = len(filenames_hashes)
-    # buckets_size = len(buckets).bit_length() - 1
-
-    buckets = [0,len(filenames_hashes)] # lol no hashmap for u
-    buckets_size = 0
-
-    logging.debug(str((buckets,buckets_size)))
-    
-    if logging.getLogger().getEffectiveLevel() == logging.DEBUG:
-        for hashy in filenames_hashes:
-            hashy = hashy % (1 << buckets_size)  
-            logging.debug(str(buckets[hashy]))
-            logging.debug(str(buckets[hashy+1]))
-    
-    packed_buckets = struct.pack(f'<{len(buckets)}H',*buckets)
-    
-    if pad_amnt := len(packed_buckets) % 4:
-        packed_buckets += b'\x00' * (4 - pad_amnt)
-    
-    vol_header = bytearray(b'\x00\xC0\xE6\x9E\x00\x00' +
-                    struct.pack('B',buckets_size) + 
-                    b'\x00\x1C\x00\x00\x00' + 
-                    struct.pack("<I", len(filenames_hashes)) + 
-                    b'\x00\x00\x00\x00' + 
-                    struct.pack("<I", datablocks_offset) + 
-                    struct.pack("<I", decompressed_data_size) +
-                    packed_buckets
-                )
-    
-
-    vol_header[16:16+4] = struct.pack('<I',len(vol_header))
-    
-    logging.debug(str(vol_header.hex(' ')))
-    logging.debug(str(len(vol_header)))
+def encode_save(decompressed_save_bytes: bytes, /):
+    WBITS = -15
+    SOMEMAGICVALUE = 1224793212
     
     
     
-    return vol_header, buckets_size
-
-def read_header(vol: BytesIO) -> tuple[int,int,int.int]:
-    """
-    reads out the required information about the header and seeks the vol to the start of the file links
-    """
-    vol.seek(0xc)
-    file_count = decode_num(vol.read(4))
-    filelinks_offset = decode_num(vol.read(4)) # offset 0x10
+    if len(decompressed_save_bytes) < 500:
+        raise ValueError(f'Does not seem to be a decompressed save {len(decompressed_save_bytes)} bytes is too small')
     
-    vol.seek(0x14)
-    datablocks_offset = decode_num(vol.read(4))    
+    new_save = zlib.compress(decompressed_save_bytes,wbits=WBITS)
     
-    vol.seek(0x18)
-    decompressed_data_size = decode_num(vol.read(4))
+    new_hash = struct.pack("<I",uint32(scurse_hash(new_save) + SOMEMAGICVALUE))
+    return new_hash + new_save
     
-    vol.seek(filelinks_offset)
     
-    return file_count,filelinks_offset,datablocks_offset,decompressed_data_size
-
-
-def extract_decompressed_vol(vol: BytesIO, output_folder: Path):
-    file_count,filelinks_offset,datablocks_offset,decompressed_data_size = read_header(vol)
-    
-    file_links = [VolumeFileLink.from_bytes(vol.read(0x18)) for _ in range(file_count)]
-    filenames = [filename.decode('ascii') for filename in vol.read((datablocks_offset) - vol.tell()).split(b'\x00') if filename]
-
-    for file_link,filename in zip(file_links,filenames):
-        assert file_link.filename_hash == scurse_hash(filename.lower().encode('ascii'))
-        vol.seek(file_link.file_data_start)
-        with open(Path(output_folder, filename),'wb') as f:
-            f.write(vol.read(file_link.file_data_size))
-
-def pack_to_decompressed_vol(vol_write_read_plus: BytesIO, output_folder: Path):
-    files = [file for file in Path(output_folder).iterdir() if file.is_file()]
-    
-    header,buckets_size = build_vol_header(0,0,{scurse_hash(filename.name.lower().encode('ascii')) for filename in files})
-    #files.sort(key = lambda filename: scurse_hash(filename.name.lower().encode('ascii')) % (1 << buckets_size))
-    files.sort(key = lambda filename: scurse_hash(filename.name.lower().encode('ascii')))
-    
-    vol_write_read_plus.write(header)
-
-    for file in files:
-        vol_write_read_plus.write(bytes(VolumeFileLink(0,0,0)))
-
-    for file in files:
-        vol_write_read_plus.write(file.name.encode('ascii') + b'\x00')
-    
-    datablocks_offset = vol_write_read_plus.tell()
-    
-    if pad_amnt := vol_write_read_plus.tell() % 32:
-        vol_write_read_plus.write(b'\x00' * (32 - pad_amnt))
-    
-    file_links = []
-
-    if logging.getLogger().getEffectiveLevel() == logging.DEBUG:
-        for index, x in enumerate(files):
-            logging.debug(str((x.name, index)))
-    
-    for index, file in enumerate(files):
-        file_data = file.read_bytes()
-        file_links.append(VolumeFileLink(scurse_hash(file.name.lower().encode('ascii')),vol_write_read_plus.tell(),len(file_data)))
-        vol_write_read_plus.write(file_data)
-        
-        if not index == len(files)-1 or len(files) == 1:
-            if pad_amnt := vol_write_read_plus.tell() % 32:
-                vol_write_read_plus.write(b'\x00' * (32 - pad_amnt))
-
-    decompressed_data_size = vol_write_read_plus.tell()
-    
-    vol_write_read_plus.seek(len(header))
-    for file_link in file_links:
-        vol_write_read_plus.write(bytes(file_link))
-
-    vol_write_read_plus.seek(0)
-    vol_write_read_plus.write(build_vol_header(datablocks_offset,decompressed_data_size,{scurse_hash(filename.name.lower().encode('ascii')) for filename in files})[0])
-
-
-def vol2files(input_vol: Path, output_folder: Path):
-    """
-    Extract a .vol file into files to ther output_folder
-    
-    :param Path input_vol: The path to the input vol file, eg Path('app\Resource\SCENE_INTRO_BOSS.vol')
-    :param Path output_folder: The path where to extract the files to, eg Path('stuff\SCENE_INTRO_BOSS')
-    """
-    with open(input_vol,'rb') as f:
-        data = BytesIO(decompress_vol(f.read()))
-    
-    extract_decompressed_vol(data,output_folder)
-
-def files2vol(input_folder: Path, output_file: Path, nvft_file: Path):
-    """
-    Pack loose files back into a .vol file, alongise patching the nvft file with the new decompressed size and compressed size
-    
-    :param Path input_vol: The path to the input folder with loose files, eg Path('stuff\SCENE_INTRO_BOSS.vol')
-    :param Path output_folder: The path to the output .vol file, eg Path('app\Resource\SCENE_INTRO_BOSS.vol')
-    :param Path nvft_file: The path to the nvft file, which needs to be patched for this to work, eg Path('app\nvft')
-    """
-    open(output_file,'w').close()
-    with open(output_file,'rb+') as f:
-        pack_to_decompressed_vol(f,input_folder)
-        f.seek(0)
-        data = f.read()
-        new_data = compress_vol(data,(Path(output_file).name,nvft_file))
-    with open(output_file,'wb') as f:
-        f.write(new_data)
-
-
-def main(args=None):
-    parser = argparse.ArgumentParser(description='Simple tool to extract .vol files, and pack files back to .vol (used in Shantae and the Pirate\'s Curse)')
-    
-    parser.add_argument('input_file',help='Input file to extract from, or folder with files to pack into')
-    parser.add_argument('output_file',help='Output location to extract the vol, or output file to pack the files into vol',nargs='?', default=1)
-
-    parser.add_argument('-p', '--pack_to_vol', help='The path to the nvft file, to patch in new offsets and lengths, if packing back into vol', nargs='?', type = str, const=False)
-
-    args = parser.parse_args(args)
-
-    if args.pack_to_vol is False:
-        raise ValueError('Please provide a path to the nvft file!')
-    
-    if args.pack_to_vol:
-        if args.output_file == 1:
-            args.output_file = Path(os.getcwd(),Path(Path(args.input_file).name).with_suffix('.vol'))
-        files2vol(args.input_file,args.output_file,args.pack_to_vol)
-    else:
-        if args.output_file == 1:
-            args.output_file = os.getcwd()
-        new_path = Path(args.output_file,Path(args.input_file).stem)
-        new_path.mkdir(exist_ok=True)
-        
-        vol2files(args.input_file,new_path)
-
 
 if __name__ == '__main__':
-    main()
+    import argparse
+    
+    parser = argparse.ArgumentParser(
+                    prog='SCurseDecPS4',
+                    description='Decompress and compress saves for Shantae and the Pirate\'s Curse, along side generating the correct hash',
+                    )
+    
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-d','-decompress',help='Decompress a compressed save, the hash is checked',action='store_true')
+    group.add_argument('-c','-compress',help='Compress a decompressed save, the correct hash is also generated',action='store_true')
+    
+
+
+    parser.add_argument('-i', '--input', help='Input save file',required=True)
+    parser.add_argument('-o', '--output', help='output save file',required=True)
+        
+    args = parser.parse_args()
+    
+    with open(args.input,'rb') as f:
+        input_data = f.read()
+    
+    output_data = decode_save(input_data) if args.d else encode_save(input_data)
+    
+    with open(args.output,'wb') as f:
+        f.write(output_data)
+    
+    if args.d:
+        print(f'Decompressed {args.input} succesfully! Decompressed save is in {args.output}')
+    elif args.c:
+        print(f'Compressed {args.input} succesfully! Compressed save is in {args.output}')
+    
+    """
+    from io import BytesIO
+    gems_offset = 0x138C
+    
+    with open('savegame','rb') as f:
+        save = BytesIO(decode_save(f.read()))
+    
+    save.seek(gems_offset)
+    
+    mygems = struct.unpack("<I",save.read(4))[0]
+    save.seek(0)
+    
+    print(mygems)
+    
+    save.seek(gems_offset)
+    
+    save.write(struct.pack("<I",786))
+    save.seek(0)
+    
+    with open('savegame.bin','wb') as f:
+        f.write(encode_save(save.getvalue()))
+    """
